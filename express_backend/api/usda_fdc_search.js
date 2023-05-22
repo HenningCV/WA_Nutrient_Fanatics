e27ff68cf89398f9c5071d46076fef8efff2b32c
HenningCV <henningvenhorst@gmail.com>
2023-05-22 14:36:36 +0200

    outsourcing funktions

---
+++ express_backend/api/usda_fdc_search.js
@@ -0,0 +1,16 @@
+let usda_key = ' Xxb4xFDH9WKXigQ6yxhYdknrwHKW2U1kcSlNhAWJ'
+
+function get_usda_fdc_search(key, query)
+{
+    fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${key}&query=${query}`)
+        .then((response) =>
+        {
+            return response.json();
+        })
+
+}
+
+function get_first_element_in_object(data)
+{
+    return data[0]
+}
