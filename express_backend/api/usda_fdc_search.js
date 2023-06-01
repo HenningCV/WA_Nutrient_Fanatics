b5c4d2d055e446774a407387e608c3525313fb60
HenningCV <henningvenhorst@gmail.com>
2023-06-01 14:19:03 +0200

    single result changes

--- express_backend/api/usda_fdc_search.js
+++ express_backend/api/usda_fdc_search.js
@@ -1,16 +1,13 @@
-let usda_key = ' Xxb4xFDH9WKXigQ6yxhYdknrwHKW2U1kcSlNhAWJ'
+const usda_key = ' Xxb4xFDH9WKXigQ6yxhYdknrwHKW2U1kcSlNhAWJ'
 
-function get_usda_fdc_search(key, query)
-{
+function get_usda_fdc_search(key, query) {
     fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${key}&query=${query}`)
-        .then((response) =>
-        {
+        .then((response) => {
             return response.json();
         })
 
 }
 
-function get_first_element_in_object(data)
-{
+function get_first_element_in_object(data) {
     return data[0]
 }
