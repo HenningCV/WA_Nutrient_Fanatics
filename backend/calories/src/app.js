f6e16d940723aaf1981b989740ef62e7035f6e11
HenningCV <henningvenhorst@gmail.com>
2023-07-02 13:12:29 +0200

    minor refactoring

--- backend/calories/src/app.js
+++ backend/calories/src/app.js
@@ -5,7 +5,7 @@ import express from 'express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 //Routers
-import {router as caloriesRouter} from './routes/calories.js';
+import {router as caloriesRouter} from './routes/caloriesRouter.js';
 
 // Variables
 const app = express();
