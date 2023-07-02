0e7e37c1c3eadd1ce468baacc637339b167e6953
Lucas / Diafreak <lucas.hengelhaupt@fh-erfurt.de>
2023-07-02 16:30:47 +0200

    display recipes-list with react

--- frontend/src/components/recipes/recipesList.jsx
+++ frontend/src/components/recipes/recipesList.jsx
@@ -2,27 +2,39 @@ import { useState, useEffect } from "react";
 
 export const RecipesList = () => {
 
-    const[recipes, setRecipes] = useState(null);
+    const [recipes, setRecipes] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError]     = useState(null);
 
     useEffect(() => {
-        fetch(`http://localhost:20073/recipes`)
+        fetch('http://localhost:20073/recipes')
             .then(response => response.json())
-            .then(response => {
-                if (response) {
-                    console.log(response);
-                    return <>
-                        <div className="recipesList">
-                            <p>I'm  a recipe list</p>
-                            {
-                                response.map(recipe => (
-                                    <p>
-                                        { recipe.toString() }
-                                    </p>
-                                ))
-                            }
-                        </div>
-                    </>;
+            .then(data => {
+                if (data) {
+                    console.log(data);
+                    setRecipes(data);
+                }
+                else {
+                    console.log("No Response from http://localhost:20073/recipes");
                 }
             })
+            .catch(err => setError(err.message))
+            .finally(() => setLoading(false))
     }, [])
+
+
+    return (
+        <div>
+            <h3>I'm  a recipe list</h3>
+            { loading && <div>Loading...</div> }
+            { error && (
+                <div>{`There is a problem fetching the post data - ${error}`}</div>
+            )}
+            { recipes && recipes.map((recipe, i) => (
+                <p key={i}>
+                    { recipe["name"] }
+                </p>
+            ))}
+        </div>
+    )
 }
